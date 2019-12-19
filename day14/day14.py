0f8e3795c943152582d336d10800d2f4064ec0c3
Matt Delmage <mdelmage@fitbit.com>
2019-12-18 20:08:11 -0800

    Day 14 Part 2

--- day14/day14.py
+++ day14/day14.py
@@ -2,6 +2,8 @@
 # coding: utf-8
 
 from collections import namedtuple
+import sys
+
 
 Ingredient = namedtuple('Ingredient', 'quantity link')
 
@@ -13,23 +15,26 @@ reserves = {}
 chemicals = set()
 ore_mined = 0
 
-def produce(chemical):
+def produce(chemical, exit_on_mine=False):
     r = reactions[chemical]
 
     # Recursion limit. Bail early.
     if CHEMICAL_BASE == chemical:
+        if exit_on_mine:
+            print reserves
+            print "We were able to make {0} units of {1}!".format(reserves[CHEMICAL_REFINED], CHEMICAL_REFINED)
+            sys.exit()
         reserves[chemical] += r.quantity
         global ore_mined
         ore_mined += 1
         return
 
     # Recurse into base chemicals
-    for input in r.inputs:
-        ingredient_name = input.link.name
-        ingredient_quantity = input.quantity
-        ingredient_reserves = reserves[ingredient_name]
+    for i in r.inputs:
+        ingredient_name = i.link.name
+        ingredient_quantity = i.quantity
         while reserves[ingredient_name] < ingredient_quantity:
-            produce(ingredient_name)
+            produce(ingredient_name, exit_on_mine)
 
         # Consume the ingredient
         reserves[ingredient_name] -= ingredient_quantity
@@ -55,11 +60,11 @@ class Reaction():
         self.name = name
     
     def __str__(self):
-        str = ""
+        string = ""
         for i in self.inputs:
-            str += "{0} units of {1} and ".format(i.quantity, i.link.name)
-        str = str[:-4] + "become {0} units of {1}".format(self.quantity, self.name)
-        return str
+            string += "{0} units of {1} and ".format(i.quantity, i.link.name)
+        string = string[:-4] + "become {0} units of {1}".format(self.quantity, self.name)
+        return string
     
     def __repr__(self):
         return self.__str__()
@@ -90,5 +95,19 @@ with open(filename, "r") as f:
         name = output.split(" ")[1]
         reactions[name].configure(inputs, output)
 
+
 produce(CHEMICAL_REFINED)
 print "We mined {0} units of {1} to produce 1 {2}!".format(ore_mined, CHEMICAL_BASE, CHEMICAL_REFINED)
+
+# Reset and add one trillion units of ore
+for chemical in reserves:
+    reserves[chemical] = 0
+reserves[CHEMICAL_BASE] = 1000 * 1000 * 1000 * 1000
+
+# Brute force attack. Ugh.
+while True:
+    produce(CHEMICAL_REFINED, True)
+    if 0 == reserves[CHEMICAL_BASE] % 1000:
+        ratio = reserves[CHEMICAL_BASE] / float(1000 * 1000 * 1000 * 1000)
+        print "{0} / {1}".format(ratio, reserves[CHEMICAL_REFINED] / (1 - ratio))
+
