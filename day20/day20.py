e01b1a822b48470f0c935e6bbc6ba262b3a6fe12
Matt Delmage <mdelmage@fitbit.com>
2019-12-25 20:46:15 -0800

    Day 20 Part 2

--- day20/day20.py
+++ day20/day20.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # coding: utf-8
 
+import copy
+
 MOVES = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 TILE_EMPTY    = ' '
@@ -18,14 +20,18 @@ maze = {}
 portals = {}
 portal_names = set()
 portal_pairs = {}
+levels = {}
 
 def is_capital_letter(l):
     return (ord(l) >= ord('A')) and (ord(l) <= ord('Z'))
 
-def print_maze(m):
+def print_maze(level):
     x = 0
     y = 0
 
+    print "Maze Level {0}".format(level)
+
+    m = levels[level]
     while (x, y) in m:
         while (x, y) in m:
             if isinstance(m[(x, y)], int):
@@ -37,40 +43,55 @@ def print_maze(m):
         y += 1
         x = 0
 
-
 def portal_exits(l):
-    if l in portal_pairs: return [portal_pairs[l]]
-    return []
+    in_level = l[0]
+    in_coord = (l[1], l[2])
+    if in_coord not in portal_pairs:
+        return []
+
+    out_coord  = portal_pairs[in_coord]
+    if in_coord[0] < 4 or in_coord[1] < 4 or in_coord[0] > maze_x - 4 or in_coord[1] > maze_y - 4:
+        out_level = in_level - 1
+        if out_level < 0: return []
+    else:
+        out_level = in_level + 1
+
+    e = (out_level, out_coord[0], out_coord[1])
+    return [e]
     
 def find_paths():
     global maze
     path_len = 0
-    paths = {path_len: [starting_coord]}
+    paths = {path_len: [(0, starting_coord[0], starting_coord[1])]}
 
     # Flood-style pathfinding:
     # Find all grid squares at a distance of 1, then 2, 3...
-    while len(paths[path_len]) > 0:
+    # Artificial path limit of 10k.
+    while len(paths[path_len]) > 0 and path_len < 10000:
         paths[path_len + 1] = []
         for l in paths[path_len]:
-            maze[l] = path_len
             # Check all four neighbors to see if they're unmapped 
-            for next_l in [(l[0] + m[0], l[1] + m[1]) for m in MOVES] + portal_exits(l):
-                if TILE_PASSAGE == maze[next_l]:
+            for coord in [(l[0], l[1] + m[0], l[2] + m[1]) for m in MOVES] + portal_exits(l):
+                level = coord[0]
+                next_coord = (coord[1], coord[2])
+                if level not in levels: levels[level] = copy.deepcopy(maze)
+                
+                if TILE_PASSAGE == levels[level][next_coord]:
                     # Empty passageway; mark it
-                    maze[next_l] = path_len + 1
-                    paths[path_len + 1].append(next_l)
-                elif isinstance(maze[next_l], int) and maze[next_l] > path_len + 1:
+                    levels[level][next_coord] = path_len + 1
+                    paths[path_len + 1].append((level, next_coord[0], next_coord[1]))
+                elif isinstance(levels[level][next_coord], int) and levels[level][next_coord] > path_len + 1:
                     # Previously mapped, but we've discovered a shorter path
-                    maze[next_l] = path_len + 1
-                    paths[path_len + 1].append(next_l)
-                elif maze[next_l] in portal_names:
+                    levels[level][next_coord] = path_len + 1
+                    paths[path_len + 1].append((level, next_coord[0], next_coord[1]))
+                elif levels[level][next_coord] in portal_names:
                     # Found a portal; jump to the other side
-                    maze[next_l] = path_len + 1
-                    paths[path_len + 1].append(next_l)
-                elif ENDING_PORTAL[0] == maze[next_l]:
+                    levels[level][next_coord] = path_len + 1
+                    paths[path_len + 1].append((level, next_coord[0], next_coord[1]))
+                elif ENDING_PORTAL[0] == levels[level][next_coord]:
                     # Found the exit!
-                    maze[next_l] = path_len + 1
-                    paths[path_len + 1].append(next_l)
+                    levels[level][next_coord] = path_len + 1
+                    paths[path_len + 1].append((level, next_coord[0], next_coord[1]))
                 #print next_l
         path_len += 1
         #break
@@ -146,10 +167,10 @@ with open("day20.txt", "r") as f:
     for p in portals:
         if STARTING_PORTAL == p:
             maze[portals[p][0]] = STARTING_PORTAL[0]
-            starting_coord = portals[p][0]
+            starting_coord = (portals[p][0][0], portals[p][0][1])
         elif ENDING_PORTAL == p:
             maze[portals[p][0]] = ENDING_PORTAL[0]
-            ending_coord = portals[p][0]
+            ending_coord = (portals[p][0][0], portals[p][0][1])
         else:
             portal_pairs[portals[p][0]] = portals[p][1]
             portal_pairs[portals[p][1]] = portals[p][0]
@@ -159,9 +180,9 @@ with open("day20.txt", "r") as f:
                 maze[coord] = portal_name
             i += 1
 
-    print_maze(maze)
+    levels[0] = copy.deepcopy(maze)
+    print_maze(0)
 
     find_paths()
-    print_maze(maze)
-    print "Shortest path out of the maze is {0} steps.".format(maze[ending_coord])
+    print "Shortest path out of the maze is {0} steps.".format(levels[0][(ending_coord[0], ending_coord[1])])
     
