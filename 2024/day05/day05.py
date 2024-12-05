5cb425b94ae23ef76b90e43dcd61d73f7de90932
Matt Delmage <54281313+mdelmage@users.noreply.github.com>
2024-12-05 07:04:49 -0800

    Start of Part 2

--- 2024/day05/day05.py
+++ 2024/day05/day05.py
@@ -5,13 +5,17 @@ def valid_update(update):
     prev_pages = set()
     for page in update:
         for (comes_before, comes_after) in rules:
-            if page == comes_before and comes_after in prev_pages: return False
+            if page == comes_before and comes_after in prev_pages: return 0
         prev_pages.add(page)
 
-    return True
+    return update[len(update) // 2]
+
+def fix_update(update):
+    print('Trying to fix {0}...'.format(update))
+    return 0
 
 # Parse the safety manual page ordering rules and update pages.
-with open('day05_input.txt') as f:
+with open('day05_test.txt') as f:
     safety_manual = [line.rstrip('\n') for line in f]
     divider = safety_manual.index('')
     rules = safety_manual[:divider]
@@ -21,8 +25,14 @@ with open('day05_input.txt') as f:
 
 # Determine which updates are already in the correct order.
 # What do you get if you add up the middle page number from those correctly-ordered updates?
-sum_middle_pages = 0
+sum_correct_middle_pages = 0
+sum_incorrect_middle_pages = 0
 for update in updates:
-    if valid_update(update): sum_middle_pages += update[len(update) // 2]
+    middle_page = valid_update(update)
+    sum_correct_middle_pages += middle_page
 
-print('Part One: Sum of middle pages of correctly-ordered updates is {0}.'.format(sum_middle_pages))
+    if middle_page == 0:
+        sum_incorrect_middle_pages += fix_update(update)
+
+print('Part One: Sum of middle pages of correctly-ordered updates is {0}.'.format(sum_correct_middle_pages))
+print('Part Two: Sum of middle pages of incorrectly-ordered updates is {0}.'.format(sum_incorrect_middle_pages))
